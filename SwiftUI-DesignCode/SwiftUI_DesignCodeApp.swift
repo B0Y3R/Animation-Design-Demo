be1b2b06ec579028dd13f43ecec01f67c4a8051d
James Boyer <jamesboyer92@user.noreply.github.com>
2021-12-28 19:30:19 -0500

    Initial Commit

---
+++ SwiftUI-DesignCode/SwiftUI_DesignCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_DesignCodeApp.swift
+//  SwiftUI-DesignCode
+//
+//  Created by James Boyer on 12/28/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_DesignCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
