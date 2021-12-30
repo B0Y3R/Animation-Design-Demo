e8454d7f31d883fefe129ff19586a20a42ad589c
James Boyer <jamesboyer92@user.noreply.github.com>
2021-12-30 17:57:49 -0500

    created animation for courseview, added custom transitions for logo to close button, added text and card that appears undeneath card when animatied

--- SwiftUI-DesignCode/CourseList.swift
+++ SwiftUI-DesignCode/CourseList.swift
@@ -26,36 +26,69 @@ struct CourseView: View {
     @State var show: Bool = false
     
     var body: some View {
-        VStack {
-            HStack(alignment: .top) {
-                VStack(alignment: .leading, spacing: 8.0) {
-                    Text("SwiftUI Advanced")
-                        .font(.system(size: 24, weight: .bold))
-                        .foregroundColor(.white)
-                    Text("20 Sections")
-                        .foregroundColor(Color.white.opacity(0.7))
-                }
+        ZStack(alignment: .top) {
+            VStack(alignment: .leading, spacing: 20) {
+                Text("Take you SwiftUI App to the app Store with advanced techniques like API data, packages and CMS")
                 
-                Spacer()
-                Image("Logo1")
+                Text("About this course")
+                    .font(.title).bold()
+                
+                Text("This course is unlike any other. We care about design and want to make sure that you get better at it in the process. It was written for designers and developers who are passionate about collaborating and buildg real apps for Ios and macOS. While it's not on codebase for all apps, you learn once and can apply the techniques and crontrols to all platforms with incredible quality, consistency  and performance. It's beginner-friendly, but it's also packed with design tricks and efficient workflows for building great user interfaces and interactions.")
+                
+                Text("Minial coding experience required, such as in HTML and CSS. Please not tht Xcode 11 and Catalina are essential. Once you get everthing installed, it'll get a lot freindlier! I added a bunch of troubleshoots at the end if this page to help you navigate the issues you might encounter.")
             }
-            Spacer()
-            Image("Card1")
-                .resizable()
-                .aspectRatio(contentMode:.fit)
-                .frame(maxWidth: .infinity)
-                .frame(height: 140, alignment: .top)
+            .padding(30)
+            .frame(maxWidth: show ? .infinity : screen.width - 60, maxHeight: show ? .infinity : 280 )
+            .offset(y: show ? 460 : 0)
+            .background(Color.white)
+            .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+            .shadow(color: Color.black.opacity(0.3), radius: 20, x: 0, y: 20)
+            .opacity(show ? 1 : 0)
+
+            
+            
+            
+            VStack {
+                HStack(alignment: .top) {
+                    VStack(alignment: .leading, spacing: 8.0) {
+                        Text("SwiftUI Advanced")
+                            .font(.system(size: 24, weight: .bold))
+                            .foregroundColor(.white)
+                        Text("20 Sections")
+                            .foregroundColor(Color.white.opacity(0.7))
+                    }
+                    
+                    Spacer()
+                    
+                    ZStack {
+                        Image("Logo1")
+                            .opacity(show ? 0 : 1)
+                        Image(systemName: "xmark")
+                            .frame(width: 40, height: 40)
+                            .background(.black)
+                            .foregroundColor(.white)
+                            .clipShape(Circle())
+                            .opacity(show ? 1 : 0)
+                    }
+                }
+                Spacer()
+                Image("Card1")
+                    .resizable()
+                    .aspectRatio(contentMode:.fit)
+                    .frame(maxWidth: .infinity)
+                    .frame(height: 140, alignment: .top)
+            }
+            .padding(show ? 30 : 20)
+            .padding(.top, show ? 30 : 0)
+            .frame(maxWidth: show ? .infinity : screen.width - 60, maxHeight: show ? 460 : 280)
+            .background(.purple)
+            .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+            .shadow(color: Color.purple.opacity(0.3), radius: 20, x: 0, y: 20)
+            .onTapGesture {
+                show.toggle()
+            }
         }
-        .padding(show ? 30 : 20)
-        .padding(.top, show ? 30 : 0)
-        .frame(maxWidth: show ? .infinity : screen.width - 60, maxHeight: show ? .infinity : 280)
-        .background(.purple)
-        .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
-        .shadow(color: Color.purple.opacity(0.3), radius: 20, x: 0, y: 20)
         .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0.2))
-        .onTapGesture {
-            show.toggle()
-        }
         .edgesIgnoringSafeArea(.all)
     }
 }
