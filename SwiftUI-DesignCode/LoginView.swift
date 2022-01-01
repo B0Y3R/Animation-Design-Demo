734b2a5903c75d674b7774c6df0fbd9dfd5c4709
James Boyer <jamesboyer92@user.noreply.github.com>
2022-01-01 15:47:05 -0500

    added blob images and created animation for them to spin in background, then added drag events and passed in viewstate to create 3d parallax animation on the card, added scaleEffect as well for a little extra umpffff

--- SwiftUI-DesignCode/LoginView.swift
+++ SwiftUI-DesignCode/LoginView.swift
@@ -8,14 +8,18 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State var show: Bool = false
+    @State var viewState: CGSize = CGSize.zero
+    @State var isDragging: Bool = false
+    
     var body: some View {
         ZStack(alignment: .top) {
-            Color.black.edgesIgnoringSafeArea(.all)
+            Color.black.edgesIgnoringSafeArea(.all) // back layer
             
-            Color("background2").edgesIgnoringSafeArea(.bottom)
+            Color("background2").edgesIgnoringSafeArea(.bottom) // middle layer
                 .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
             
-            VStack {
+            VStack {                                                                        // top layer
                 GeometryReader { geometry in                                                // wrap Text in a geometry reader
                     Text("Learn design & code.\nFrom scratch.")
                         .font(.system(size: geometry.size.width / 10, weight: .bold))       // take screen size into account when displaying title fonts
@@ -23,13 +27,15 @@ struct LoginView: View {
                 .frame(maxWidth: 375, maxHeight: 100) // ideal max font size
                 .padding(.horizontal, 16)
                 .foregroundColor(Color("background1"))
-
+                .offset(x: viewState.width / 15, y: viewState.height / 15)
+                
                 
                 Text("80 hours of courses for SwiftUI, React, and design tools.")
                     .font(.subheadline)
                     .frame(width: 250)
                     .foregroundColor(.primary)
-
+                    .offset(x: viewState.width / 20, y: viewState.height / 20)
+                
                 Spacer()
             }
             .multilineTextAlignment(.center)
@@ -37,22 +43,43 @@ struct LoginView: View {
             .frame(height: 477)
             .frame(maxWidth: .infinity)
             .background(
-                ZStack {
+                ZStack {                                                                // build out two images, blobs in this instance
                     Image("Blob")
                         .offset(x: -150, y: -200)
-                    .blendMode(.plusDarker)
+                        .rotationEffect(Angle(degrees: show ? 360+90 : 90))             // added rotation effect, +90 so it doesnt skip
+                        .blendMode(.plusDarker)
+                        .onAppear {
+                            self.show = true
+                        }
                     Image("Blob")
-                        .offset(x: -200, y: -250)
+                        .offset(x: -200, y: -50)
                         .blendMode(.overlay)
+                        .rotationEffect(Angle(degrees: show ? 360+90 : 0), anchor: .topLeading)
                 }
+                    .animation(Animation.linear(duration: 40).repeatForever())    // animation for the zStack with both blobs, animating the rotation effect
             )
             .background(
-                
-                Image("Card3"),
+                Image("Card3")
+                    .offset(x: viewState.width / 20, y: viewState.height / 20)
+                ,
                 alignment: .bottom
             )
             .background(Color("card1"))
             .clipShape(RoundedRectangle(cornerRadius: 30, style: .continuous))
+            .scaleEffect(isDragging ? 0.8 : 1)
+            .animation(.timingCurve(0.2, 0.8, 0.2, 1, duration: 0.8))
+            .rotation3DEffect(Angle(degrees: 5), axis: (x: viewState.width, y: viewState.height, z: 0))
+            .gesture(
+                DragGesture()
+                    .onChanged { value in
+                        self.isDragging = true
+                        self.viewState = value.translation
+                    }
+                    .onEnded { value in
+                        self.isDragging = false
+                        self.viewState = .zero
+                    }
+            )
         }
     }
 }
